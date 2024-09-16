4039ee8238d3db936884e034d0c32835f4611aff
Vachek-hooves <vachek0602@gmail.com>
2024-09-16 17:53:14 +0300

    updates MainScreen

--- data/data.js
+++ data/data.js
@@ -3,30 +3,66 @@ export const Starlight = [
     name: 'Ursa Major',
     id: 's1',
     position: [
-      { id: 1, xFactor: -0.8, yFactor: -0.9 },
-      { id: 2, xFactor: -0.6, yFactor: -0.4 },
-      { id: 3, xFactor: -0.4, yFactor: 0.1 },
-      { id: 4, xFactor: 0, yFactor: 0.1 },
-      { id: 5, xFactor: 0.3, yFactor: 0.4 },
-      { id: 6, xFactor: 0.6, yFactor: -0.1 },
-      { id: 7, xFactor: 0.3, yFactor: -0.4 },
+      {id: 1, xFactor: -0.8, yFactor: -0.9},
+      {id: 2, xFactor: -0.6, yFactor: -0.4},
+      {id: 3, xFactor: -0.4, yFactor: 0.1},
+      {id: 4, xFactor: 0, yFactor: 0.1},
+      {id: 5, xFactor: 0.3, yFactor: 0.4},
+      {id: 6, xFactor: 0.6, yFactor: -0.1},
+      {id: 7, xFactor: 0.3, yFactor: -0.4},
     ],
-    connections: [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]
+    connections: [
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+    ],
   },
   {
     name: 'Lyra',
     id: 's2',
     position: [
-      { id: 1, xFactor: -0.6, yFactor: 0 },      // Vega (brightest star)
-      { id: 2, xFactor: -0.6, yFactor: 0.9 },  // Sheliak
-      { id: 3, xFactor: -0.1, yFactor: 0.9 },  // Sulafat
-      { id: 4, xFactor: 0.35, yFactor: -0.7 }, // Delta Lyrae
-      { id: 5, xFactor: -0.2, yFactor: -0.3 }, // Zeta1 Lyrae
+      {id: 1, xFactor: -0.6, yFactor: 0}, // Vega (brightest star)
+      {id: 2, xFactor: -0.6, yFactor: 0.9}, // Sheliak
+      {id: 3, xFactor: -0.1, yFactor: 0.9}, // Sulafat
+      {id: 4, xFactor: 0.35, yFactor: -0.7}, // Delta Lyrae
+      {id: 5, xFactor: -0.2, yFactor: -0.3}, // Zeta1 Lyrae
     ],
-    connections: [[1, 2], [2, 3], [3, 4], [4, 5], [5, 1]]
+    connections: [
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 1],
+    ],
   },
   {name: 'Gemini', id: 's3', position: []},
-  {name: 'Libra', id: 's4', position: []},
+  {
+    name: 'Libra',
+    id: 's4',
+    position: [
+      {id: 1, xFactor: -0.9, yFactor: -0.4}, // Top left
+      {id: 2, xFactor: -0.6, yFactor: -0.3}, // Top right
+      {id: 3, xFactor: -0.2, yFactor: -0.6}, // Middle left
+      {id: 4, xFactor: 0.1, yFactor: -1.2}, // Middle right
+      {id: 5, xFactor: 0.8, yFactor: -0.9}, // Bottom left
+      {id: 6, xFactor: 0.7, yFactor: -0.1}, // Bottom right
+      {id: 7, xFactor: 0.2, yFactor: 0.2}, // Extended top left
+      {id: 8, xFactor: 0.3, yFactor: 0.4}, // Extended top right
+    ],
+    connections: [
+      [1, 2],
+      [1, 3],
+      [2, 4],
+      [3, 5],
+      [4, 6],
+      [5, 6],
+      [1, 7],
+      [2, 8],
+    ],
+  },
   {name: 'Scorpius', id: 's5', position: []},
   {name: 'Aries', id: 's6', position: []},
   {name: 'Cancer', id: 's7', position: []},
