6a8bd0564adae6741c04720c52714a2d02404d49
Vachek-hooves <vachek0602@gmail.com>
2024-09-16 19:51:21 +0300

    updates data

--- data/data.js
+++ data/data.js
@@ -198,5 +198,25 @@ export const Starlight = [
       [9, 10],
     ],
   },
-  {name: 'Pisces', id: 's10', position: []},
+  {
+    name: 'Pisces',
+    id: 's10',
+    position: [
+      {id: 1, xFactor: 0, yFactor: -1.4},
+      {id: 2, xFactor: -0.1, yFactor: -1.0},
+      {id: 3, xFactor: -0.3, yFactor: -0.6},
+      {id: 4, xFactor: -0.6, yFactor: 0},
+      {id: 5, xFactor: -0.1, yFactor: -0.1},
+      {id: 6, xFactor: 0.2, yFactor: -0.1},
+      {id: 7, xFactor: 0.6, yFactor: 0},
+    ],
+    connections: [
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+    ],
+  },
 ];
