cfa125cb9484ff7ae06321e1b346be1d53d91e6b
Vachek-hooves <vachek0602@gmail.com>
2024-09-17 18:14:46 +0300

    create welcome screen

--- screen/WelcomeScreen.js
+++ screen/WelcomeScreen.js
@@ -1,12 +1,19 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Animated, StyleSheet, Text, View} from 'react-native';
+import {useRef, useEffect} from 'react';
+import {MainLayout} from '../components/layout';
 
 const WelcomeScreen = () => {
-  return (
-    <View>
-      <Text>WelcomeScreen</Text>
-    </View>
-  );
+  const animation = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start(() => navigation.navigate('MainScreen'));
+
+  }, [animation]);
+
+  return <MainLayout></MainLayout>;
 };
 
 export default WelcomeScreen;
